89bff033b49e045b769e721778921c76b134d563
alonmir <alonm1103@gmail.com>
2025-10-26 10:57:10 +0200

    ci: trigger deploy (no code changes)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.get("/")
 def root():
     return "OK", 200
-
-if __name__ == "__main__":
-    # מאזין החוצה על 0.0.0.0:5000
-    app.run(host="0.0.0.0", port=5000)
 
